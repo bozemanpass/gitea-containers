0bdf59c8c7150cf43f8429fb3f900bd47eec2af2
Thomas E Lackey <telackey@bozemanpass.com>
2025-02-26 14:41:26 -0600

    External container definitions.

--- act-runner/build.sh
+++ act-runner/build.sh
@@ -2,4 +2,4 @@
 source ${BPI_CONTAINER_BASE_DIR}/build-base.sh
 # Build a local version of the act-runner image
 # TODO: enhance the default build code path to cope with this container (repo has an _ which needs to be converted to - in the image tag)
-docker build --target dind -t bpi/act-runner:stack -f ${BPI_REPO_BASE_DIR}/act_runner/Dockerfile ${build_command_args} ${BPI_REPO_BASE_DIR}/act_runner
+docker build --target dind -t ${BPI_IMAGE_NAME}:${BPI_IMAGE_LOCAL_TAG} -f ${BPI_REPO_BASE_DIR}/act_runner/Dockerfile ${build_command_args} ${BPI_REPO_BASE_DIR}/act_runner
