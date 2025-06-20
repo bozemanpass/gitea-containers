d2d9612bef1965d023e743cd855f383371c84659
Thomas E Lackey <telackey@bozemanpass.com>
2025-06-20 13:12:37 -0500

    BPI_ -> STACK_

--- act-runner/build.sh
+++ act-runner/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-source ${BPI_CONTAINER_BASE_DIR}/build-base.sh
+source ${STACK_CONTAINER_BASE_DIR}/build-base.sh
 # Build a local version of the act-runner image
-# TODO: enhance the default build code path to cope with this container (repo has an _ which needs to be converted to - in the image tag)
-docker build --target dind -t ${BPI_IMAGE_NAME}:${BPI_IMAGE_LOCAL_TAG} -f ${BPI_REPO_BASE_DIR}/act_runner/Dockerfile ${build_command_args} ${BPI_REPO_BASE_DIR}/act_runner
+
+FULL_REPO_PATH=${STACK_REPO_BASE_DIR}/gitea.com/gitea/act_runner/
+
+docker build --target dind -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${FULL_REPO_PATH}/Dockerfile ${build_command_args} ${FULL_REPO_PATH}
